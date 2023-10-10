624acfa45a621e5857fdb9d4f1bec4c5efe9c2dd
jusylopes <jusylopes@outlook.com>
2023-10-10 04:25:59 -0300

    refactor: theme

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -5,30 +5,22 @@ import 'package:flutter_contact_book/utils/no_transition_screen.dart';
 
 abstract class AppTheme {
   static ThemeData get light => ThemeData(
-      primarySwatch: AppColors.colorPrimarySwatch,
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-      fontFamily: AssetsManager.fontFamily,
-      scaffoldBackgroundColor: AppColors.colorBackground,
-      textTheme: _buildTextTheme(),
-      pageTransitionsTheme: PageTransitionsTheme(
-        builders: {
-          TargetPlatform.android: NoTransitionsScreen(),
-          TargetPlatform.iOS: NoTransitionsScreen(),
-        },
-      ),
-      appBarTheme:
-          const AppBarTheme(backgroundColor: Colors.transparent, elevation: 0),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          textStyle: const TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.w600,
-          ),
+        primarySwatch: Colors.blueGrey,
+        fontFamily: AssetsManager.fontFamily,
+        scaffoldBackgroundColor: AppColors.colorBackground,
+        textTheme: _buildTextTheme(),
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: NoTransitionsScreen(),
+            TargetPlatform.iOS: NoTransitionsScreen(),
+          },
         ),
-      ));
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          toolbarHeight: 80,
+        ),
+      );
 
   static TextTheme _buildTextTheme() {
     return const TextTheme(
@@ -38,12 +30,12 @@ abstract class AppTheme {
         color: Colors.black,
       ),
       titleMedium: TextStyle(
-        fontSize: 14.0,
+        fontSize: 20.0,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       ),
       titleSmall: TextStyle(
-        fontSize: 12.0,
+        fontSize: 18.0,
         fontWeight: FontWeight.w300,
         color: Colors.black,
       ),
